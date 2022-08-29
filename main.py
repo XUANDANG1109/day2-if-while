b5917903e67c731ce620865ab140ed3777f739d0
XUANDANG1109 <dangthilinhxuan@gmail.com>
2022-08-29 14:31:43 +0300

    day2

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Ex1
 legal = False
 
 print("A")
@@ -6,6 +7,7 @@ if legal:
     print("C")
 print("D")
 
+# Ex2
 age = int(input("what is your age?"))
 
 print("A")
@@ -14,20 +16,68 @@ if age >= 10:
     print("C")
 print("D")
 
+# Ex3
 age = int(input("what is your age?"))
 print("A")
 if age >= 10:
     print("B")
-    if age >=100:
+    if age >= 100:
         print("C")
     print("D")
 print("E")
 
+# Ex4
 age = int(input("what is your age?"))
 print("A")
 if age >= 10:
     print("B")
-if age >=100:
+if age >= 100:
     print("C")
     print("D")
 print("E")
+
+# Ex5
+secret_code = input("What is the secret code?")
+
+if secret_code == "NATO":
+    print("correct")
+print("not correct")
+
+# Ex 6
+secret_code = "NATO"
+answer = input("what is the secret code?")
+
+if secret_code == answer:
+    print("correct")
+else:
+    print("not correct")
+
+#Ex 7
+secret_code1 = "NATO"
+secret_code2 = "EU"
+
+answer = input("what is the secret code?")
+if answer == secret_code1 and answer == secret_code2:
+    print("correct")
+else:
+    print("not correct")
+
+#Ex 8
+secret_code1 = "NATO"
+secret_code2 = "EU"
+
+answer = input("what is the secret code?")
+if answer == secret_code1 or answer == secret_code2:
+    print("correct")
+else:
+    print("not correct")
+
+# Ex 8
+secret_code1 = "NATO"
+secret_code2 = "EU"
+
+answer = input("what is the secret code?")
+if answer == secret_code1 and answer == secret_code2 or 1 == 1:
+    print("correct")
+else:
+    print("not correct")
