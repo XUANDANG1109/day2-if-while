13866c93ac3f7c81055065dd7e55b847c82f3aa3
XUANDANG1109 <dangthilinhxuan@gmail.com>
2022-08-29 13:37:53 +0300

    day2

---
+++ main.py
@@ -0,0 +1,33 @@
+legal = False
+
+print("A")
+if legal:
+    print("B")
+    print("C")
+print("D")
+
+age = int(input("what is your age?"))
+
+print("A")
+if age >= 10:
+    print("B")
+    print("C")
+print("D")
+
+age = int(input("what is your age?"))
+print("A")
+if age >= 10:
+    print("B")
+    if age >=100:
+        print("C")
+    print("D")
+print("E")
+
+age = int(input("what is your age?"))
+print("A")
+if age >= 10:
+    print("B")
+if age >=100:
+    print("C")
+    print("D")
+print("E")
